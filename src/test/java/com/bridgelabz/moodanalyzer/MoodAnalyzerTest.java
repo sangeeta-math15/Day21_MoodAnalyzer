2368a460610fb22e83fa95a2859c28b75862922b
sngtmth <sngtmth@gmail.com>
2021-10-29 16:59:28 +0530

    Added:Repeat the TestCase 1.1

--- src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 public class MoodAnalyzerTest {
     @Test
     public void given_Mood_Should_Return_Happy() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood = moodAnalyzer.analyseMood("Iam in Any Mood");
-        Assert.assertEquals("HAPPY", mood);
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("Iam in Sad Mood");
+        String mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals("SAD", mood);
     }
 }
 
