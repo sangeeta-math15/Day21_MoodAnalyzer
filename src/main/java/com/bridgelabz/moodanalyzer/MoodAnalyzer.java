a99dc8958ff0e9583b9414663141c82b371105e3
sngtmth <sngtmth@gmail.com>
2021-10-29 21:56:59 +0530

    Added:Message Should Return SAD Mood

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -1,14 +1,11 @@
 package com.bridgelabz.moodanalyzer;
 
 public class MoodAnalyzer {
-    private String message;
-    public MoodAnalyzer(){
+    public MoodAnalyzer() {
     }
-    public MoodAnalyzer(String message) {
-        this.message = message;
-    }
-    public String analyseMood() {
-        if (this.message.contains("Sad"))
+
+    public String analyseMood(String message) {
+        if (message.contains("Sad"))
             return "SAD";
         else
             return "HAPPY";
